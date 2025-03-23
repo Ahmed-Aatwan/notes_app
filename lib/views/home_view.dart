0c9e6b2b1e9b2ddff8925861d8589245904aab08
Aatwan <ahmedatwaan2@gmail.com>
2025-03-23 05:55:16 +0200

    adding custom app bar

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/notes_view_body.dart';
+
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
   static String id = 'HomePage';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(body: NotesViewBody());
   }
 }
+
---
+++ lib/widgets/custom_search_icon.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {},
+      icon: const Icon(Icons.search, size: 28),
+      style: const ButtonStyle(
+        backgroundColor: WidgetStatePropertyAll(Color(0xff3b3b3b)),
+        shape: WidgetStatePropertyAll(
+          RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16))),
+        ),
+      ),
+    );
+  }
+}
