d8047c68ecd011a8be702bae0b9d18ffd4cb5387
Aatwan <ahmedatwaan2@gmail.com>
2025-03-24 18:13:35 +0200

    editing textfield to use maxlines instead of contentpadding

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -17,14 +18,14 @@ class AddNoteBottomSheet extends StatelessWidget {
           CustomTextField(
             controller: contentController,
             hintText: 'Content',
-            contentPadding: const EdgeInsets.symmetric(vertical: 50, horizontal: 12),
+            maxLines: 5,
           ),
           const Spacer(flex: 1),
           ElevatedButton(
             onPressed: () {},
             style: ElevatedButton.styleFrom(
               foregroundColor: Colors.black,
-              backgroundColor: const Color(0xff60f0e7),
+              backgroundColor: kprimaryColor,
               minimumSize: const Size(double.maxFinite, 50),
             ),
             child: const Text('Add', style: TextStyle(fontSize: 18)),
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, this.contentPadding, required this.hintText, required this.controller});
-  final EdgeInsetsGeometry? contentPadding;
+  const CustomTextField({super.key, this.maxLines = 1, required this.hintText, required this.controller});
+  final int? maxLines;
   final String hintText;
   final TextEditingController controller;
   @override
   Widget build(BuildContext context) {
     return TextField(
+      keyboardType: TextInputType.emailAddress,
+      maxLines: maxLines,
       decoration: InputDecoration(
-        contentPadding: contentPadding,
-        hintStyle: const TextStyle(color: Color(0xff60f0e7)),
+        hintStyle: const TextStyle(color: kprimaryColor),
         hintText: hintText,
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(kprimaryColor),
       ),
     );
   }
+
+  OutlineInputBorder buildBorder([Color? color]) => OutlineInputBorder(borderRadius: BorderRadius.circular(16),borderSide:  BorderSide(color:color ?? Colors.white),);
 }
