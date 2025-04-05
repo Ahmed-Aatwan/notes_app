74001476460e3a30de25c330226541df09f7465d
Aatwan <ahmedatwaan2@gmail.com>
2025-04-06 01:26:30 +0200

    fixing modal hud & singlChildScrollView issue

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
@@ -52,7 +53,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
                 NoteModel noteModel = NoteModel(
                   noteTitle: titleController.text,
                   noteContent: contentController.text,
-                  noteDate: DateTime.now().day.toString(),
+                  noteDate: DateFormat('MMMM d,y').format(DateTime.now()),
                   color: Colors.blue.toARGB32(),
                 );
                 BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
